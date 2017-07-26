64a7c2a483448fb349a239e6aa53b44455b295a3
vbushuev <v.bushuev@gmail.com>
2017-07-27 01:53:57 +0300

    fix it

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>::</title>
+	<meta name="robots" content="index, all">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link href="//l.gauzymall.com/css/img/logo_garan24.png" rel="icon" type="image/x-icon">
+	<link href="//l.gauzymall.com/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="//l.gauzymall.com/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<!--<link rel="stylesheet" href="//l.gauzymall.com/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">-->
+
+	<link rel="Stylesheet" href="//l.gauzymall.com/css/jquery-ui.min.css" type="text/css">
+	<link rel="Stylesheet" href="//l.gauzymall.com/css/co.css" type="text/css">
+	<link rel="Stylesheet" href="//l.gauzymall.com/css/orders.css" type="text/css">
+
+	<script src="//l.gauzymall.com/js/jquery-2.1.4.min.js"></script>
+	<script src="//l.gauzymall.com/js/bootstrap.min.js"></script>
+	<script src="//l.gauzymall.com/js/jquery-ui.min.js"></script>
+	<script src="//l.gauzymall.com/js/jquery.color.js"></script>
+	<script src="//l.gauzymall.com/js/jquery.redirect.js"></script>
+	<script src="//l.gauzymall.com/js/jquery.maskedinput.min.js"></script>
+	<script src="//l.gauzymall.com/js/api/2.0/garan24.core.js"></script>
+	<script src="//l.gauzymall.com/js/api/2.0/garan24.delivery.js"></script>
+	<script src="//l.gauzymall.com/js/api/2.0/garan24.customer.js"></script>
+	<script src="//l.gauzymall.com/js/api/2.0/garan24.cart.js"></script>
+</head>
+<body>
+    <div class="gm-line" style="height:50px">
+
+    </div>
+    <iframe id="iframe-content" src="http://geox.com" width="100%" height="100%"></iframe>
+    <script>
+        $(document).ready(function(){
+            $("#iframe-content").append('<script>console.debug("imspy.")</script>');
+        });
+    </script>
+</body>
+</html>
